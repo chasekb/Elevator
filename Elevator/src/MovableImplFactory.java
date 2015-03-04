b371355ac1a28caec5019ac78b50411be17e9482
chasekb <chasekb@users.noreply.github.com>
2015-03-04 15:01:15 -0600

    Delete MovableImplFactory.java

--- Elevator/src/MovableImplFactory.java
+++
@@ -1,7 +0,0 @@
-import exceptions.InvalidDataException;
-
-public class MovableImplFactory {
-    public static Movable createMovable(int dest, int spd) throws InvalidDataException {
-        return new MovableImpl(dest, spd);
-    }
-}
